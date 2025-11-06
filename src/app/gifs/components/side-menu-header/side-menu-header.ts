1c750a69daebd329a6e2981aeb60904e45cb1fbe
Cesar Cortes <cesarcortes4@outlook.com>
2025-11-06 13:24:47 -0500

    Implement side menu structure and routing for dashboard, trending, and search pages

---
+++ src/app/gifs/components/side-menu-header/side-menu-header.ts
@@ -0,0 +1,9 @@
+import {  Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-side-menu-header',
+  imports: [],
+  templateUrl: './side-menu-header.html',
+
+})
+export class SideMenuHeader { }
